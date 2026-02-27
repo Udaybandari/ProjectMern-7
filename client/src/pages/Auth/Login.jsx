8af85f510567e361dbd88724fbd1f116fb1ec6be
Uday bandari <udaybandari593@gmail.com>
2026-02-27 19:00:52 +0530

    updated

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -1,7 +1,61 @@
-import React from "react";
-
+import React, { useState } from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import {Link, useNavigate} from "react-router-dom";
+import Input from "../../components/inputs/Input";
+import { validateEmail } from "../../utils/helper";
 const Login = () => {
-  return <div>Login</div>;
+  const[email,setEmail]=useState("");
+  const[name,setName]=useState("");
+  const[error,setError]=useState(null);
+  const[password,setPassword]=useState("");
+  const navigate=useNavigate();
+  const handleLogin=async (e)=>{
+    e.preventDefault();
+    if(!validateEmail(email))
+    {
+      setError("Please enter a valid email address");
+      return;
+    }
+    if(!password)
+    {
+      setError("Please enter the password");
+      return;
+    }
+    setError("");
+  }
+
+  return (
+    <AuthLayout>
+     <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center gap-1">
+       <div>
+        <h3 className="text-3xl font-bold text-black">Welcome Back</h3>
+       <p className="text-sm text-slate-700  ">Please enter your details to log in</p>
+       </div>
+       <Input
+       value={email}
+       onChange={({target})=>setEmail(target.value)}
+       label="Email Address"
+       placeholder="john@example.com"
+       type="text"
+       />
+        <Input
+       value={password}
+       onChange={({ target }) => setPassword(target.value)}
+       label="Password"
+       placeholder="Min 8 Characters"
+       type="password"
+       />
+       {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+       <button type="submit" className="btn-primary" onClick={handleLogin}>LOGIN</button>
+       <p>Dont't have an account?
+        <Link className="font-medium text-primary underline" to="/signup">
+        SignUp
+        </Link>
+       </p>
+      
+     </div>
+  </AuthLayout>
+  )
 };
 
 export default Login;
--- client/src/pages/Auth/Signup.jsx
+++ client/src/pages/Auth/Signup.jsx
@@ -1,7 +1,40 @@
 import React from "react";
-
+import AuthLayout from '../../components/layouts/AuthLayout'
+import ProfilePhotoSelector from "../../components/inputs/ProfilePhotoSelector";
 const Signup = () => {
-  return <div>Signup</div>;
+  const[profilePic,setProfilePic]=useState(null);
+  const[fullName,setFullName]=useState("");
+  const[email,setEmail]=useState("");
+  const[password,setPassword]=useState("");
+  const[adminInviteToken,setAdminInviteToken]=useState("");
+  const[error,setError]=useState(null);
+ const handleSignUp=async (e)=>{
+     e.preventDefault();
+     if(!fullName(email))
+     {
+       setError("Please enter full name");
+       return;
+     }
+     if(!password)
+     {
+       setError("Please enter the password");
+       return;
+     }
+     setError("");
+   }
+  return (
+    
+    <AuthLayout>
+       <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Create an Account</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">Join us today by entering your details below</p>
+        <form onSubmit={handleSignUp}>
+          <ProfilePhotoSelector image={profilePic} setImage={setProfilePic}/>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4"></div>
+        </form>
+       </div>
+    </AuthLayout>
+  )
 };
 
 export default Signup;
